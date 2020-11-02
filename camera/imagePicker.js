cf96cc201fbaf4c95e509d1b7df5b547566e7e31
nss27 <ghtjd8582@gmail.com>
2020-11-02 14:06:50 +0900

    ImagePicker 앱 통신 추가

--- camera/imagePicker.js
+++ camera/imagePicker.js
@@ -1,4 +1,32 @@
 $(() => {
-  let imgH = '100px';
-  $('img').css({'width', '100%', 'height', imgH});
+  var data = {
+    func: ''
+  };
+
+  // 사진 업로드 클릭 이벤트
+  $('#uploadImage').on('click', () => {
+    data.func = 'uploadImage';
+    window.ReactNativeWebView.postMessage(JSON.stringify(data));
+  });
+
+  // ReactNative 통신
+  document.addEventListener('message', function(e){
+    var data = JSON.parse(e.data);
+
+    if(isNull(data)) return false;
+
+    switch(data.func)
+    {
+      // img data 송신
+      case 'updateImage':
+
+        // 이미지 data 가져오기
+        $('img').attr('src', data.uri);
+
+        // 이미지 높이 조절
+//        $('img').css({'height', '500px'});
+
+        break;
+    }
+  });
 });
