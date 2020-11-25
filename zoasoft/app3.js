f0827ab1d30a8d5bde710624faa8889e19f4974b
nss27 <ghtjd8582@gmail.com>
2020-11-25 11:47:33 +0900

    gps 기능 구현

---
+++ zoasoft/app3.js
@@ -0,0 +1,95 @@
+$(()=>{
+    // 부모창에서 받아온 수신
+    window.addEventListener('message', function(e){
+        console.log(e.data);
+
+        // 수신용 함수
+        switch (e.data.func) {
+            // GPS (NO LIVE)
+            case 'gps1Callback':
+                gps1Callback(e);
+                break;
+        
+            // GPS (LIVE)
+            case 'gps2Callback':
+                gps2Callback(e);
+                break;
+            
+            default:
+                break;
+        }
+    });
+
+    $('#gps1').on('click', gps1);
+    $('#gps2').on('click', gps2);
+    $('#camera1').on('click', camera1);
+    $('#camera2').on('click', camera2);
+});
+
+var gps1 = function()
+{
+    var data = {
+        func: 'getCurrentPosition'
+    }
+
+    // 부모창에 데이터 송신
+    window.parent.postMessage(data, '*');
+}
+
+var gps1Callback = function(e)
+{
+    var html = '위도: ' + e.data.result.lat;
+    html += '<br>';
+    html += '경도: ' + e.data.result.long;
+
+    $('#gps1Result').html(html);
+}
+
+var gpsFlug = false;
+
+var gps2 = function()
+{
+    var data = {
+        func: gpsFlug ? 'clearWatch' : 'watchPosition'
+    }
+
+    // 부모창에 데이터 송신
+    window.parent.postMessage(data, '*');
+
+    // flug change
+    gpsFlug = !gpsFlug;
+
+    // button css change
+    $('#gps2').css({
+        color: gpsFlug ? 'blue' : 'black'
+    });
+}
+
+var gps2Callback = function(e)
+{
+    var html = '위도: ' + e.data.result.lat;
+    html += '<br>';
+    html += '경도: ' + e.data.result.long;
+
+    $('#gps2Result').html(html);
+}
+
+var camera1 = function()
+{
+    var data = {
+        text: 'camera1 클릭'
+    }
+
+    // 부모창에 데이터 송신
+    window.parent.postMessage(data, '*');
+}
+
+var camera2 = function()
+{
+    var data = {
+        text: 'camera2 클릭'
+    }
+
+    // 부모창에 데이터 송신
+    window.parent.postMessage(data, '*');
+}
